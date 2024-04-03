3dcb76ab82177537edee8415ee92da4f9b224a2a
Barbora Fabianova <BARBORKA.FABIANOVA@GMAIL.COM>
2024-04-03 17:07:48 +0000

    fetch function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
+   /* $("#stream1_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
-   });
+   });*/
+
+   $("#stream1_btn").click( function() {
+    $(".stream1").show("slow"); //$(".stream1").hide("slow");
+    /*$(".stream1").show("medium"); //$(".stream1").hide("medium");
+    $(".stream1").show("fast"); //$(".stream1").hide("fast");
+    $(".stream1").show(1000); //number of miliseconds*/
+});
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
